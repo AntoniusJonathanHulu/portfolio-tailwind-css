2c668f2287546274ce96ba895f16c61c2886103d
AntoniusJonthanHulu <jonathanhulu10@gmail.com>
2025-04-29 09:46:24 +0700

    update darkmode toggle

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
 // Navbar Fixed
 window.onscroll = function () {
-  const header = document.querySelector('header');
+  const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
-    header.classList.add('navbar-fixed');
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
-    header.classList.remove('navbar-fixed');
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
-// Hamburger & Navbar
+// Hamburger
 const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector('#nav-menu');
+const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle('hidden');
+  navMenu.classList.toggle("hidden");
 });
+
+// Click outside the hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Move the toggle position according to the mode
+darkToggle.checked =
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches);
